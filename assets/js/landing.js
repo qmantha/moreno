c1a64bd6247d0092d856106124ec23f80a6f2bf5
Qmantha <111574551+qmantha@users.noreply.github.com>
2024-12-03 10:44:17 +0700

    Delete assets directory

--- assets/js/landing.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    document.getElementById('loadingScreen').style.display = 'none';
-    document.getElementById('mainContent').style.display = 'block';
-  }, 4000); // Adjust timing to match fadeOut animation
-});
